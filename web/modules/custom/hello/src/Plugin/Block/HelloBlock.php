10f06f6d5566d9cd869f0c0ad51e2d6389b79695
soikhat <hadidja.mohamed04@gmail.com>
2019-08-18 00:25:05 +0200

    creation helloblock et gestion de sa mise en cache

---
+++ web/modules/custom/hello/src/Plugin/Block/HelloBlock.php
@@ -0,0 +1,35 @@
+<?php
+namespace Drupal\hello\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+* Provides a hello block
+* @Block(
+*   id = "hello_block",
+*   admin_label = @translation("Hello")
+* )
+* @package Drupal\hello\Plugin\Block
+*/
+class HelloBlock extends BlockBase {
+
+  /*
+  * Implements Drupal\Core\Block\BlockBase::build()
+  */
+
+  public function build(){
+
+    $temps = \Drupal::Time()->getCurrentTime();
+    $user = \Drupal::CurrentUser()->getAccountName() ;
+    $formatemps = \Drupal::Service('date.formatter')->format($temps,'Y-m-d\TH:i:s');
+
+    $build = ["#markup"=>t('Welcome on our website Mr %user. It is %temps',['%temps'=> $formatemps, '%user'=>$user]),
+              "#cache"=>[
+                'keys'=>['helloblock.cache'],
+                'contexts'=>['user','timezone'],
+                'max-age'=>'10'
+                ]];
+
+    return $build;
+  }
+}
