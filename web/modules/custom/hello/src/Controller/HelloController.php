522ff49a8485cfd1c13107f2f04cf34872ca6b47
soikhat <hadidja.mohamed04@gmail.com>
2019-08-20 01:06:31 +0200

    Update HelloController.php

--- web/modules/custom/hello/src/Controller/HelloController.php
+++ web/modules/custom/hello/src/Controller/HelloController.php
@@ -8,6 +8,7 @@ class HelloController extends ControllerBase{
 
   public function content($param){
 
+    
     $name = $this-> currentUser()->getAccountName();
     return ["#markup"=>t('Welcome to the Hello page Mr/Mrs %name <br> aux parametres %param',['%name'=>$name,'%param'=>$param])];
   }
